49dceea7d7e063e9f79042c91876792db960d197
JackJayJames <115401650+JackJayJames@users.noreply.github.com>
2022-10-16 17:43:15 +0200

    done
    replace number divisible 3 and 5 by FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,13 +2,17 @@ let num = Number(prompt("enter number for fizzbuzz"));
 
 for(let i = 1; i < num+1; i++)
 {
-    if((i % 3) == 0)
+    if(((i % 3) == 0) && ((i % 5) != 0))
     {
-        console.log("Fizz");
+        console.log("Fizz");  /*replace number divisible by 3 with fizz*/
     }
-    else if((i % 5) == 0)
+    else if(((i % 5) == 0) && ((i % 3) != 0))
     {
-        console.log("Buzz");
+        console.log("Buzz");  /*replace number divisible by 5 with buzz*/
+    }
+    else if (((i % 3) == 0) && ((i % 5) == 0))
+    {
+        console.log("FizzBuzz");  /*replace number divisible by 3 and 5 with FizzBuzz*/
     }
     else
     {
